5bf4db014f411714de3cfd02bc8560803f8c24dc
Javier1213767 <JavierAndres.1213767@correo.uis.edu.co>
2021-11-17 19:24:19 -0500

    Actulización Logo

--- app/src/main/java/com/ximena/mfoodyapp/MainActivity.java
+++ app/src/main/java/com/ximena/mfoodyapp/MainActivity.java
@@ -22,13 +22,30 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText correoInicio;
     private EditText contraseñaInicio;
+    private Button mButtonLogin;
 
+    private String email = "";
+    private String contraseña = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         correoInicio=findViewById(R.id.CorreoInicioS);
         contraseñaInicio=findViewById(R.id.ContraseñaInicioS);
+        mButtonLogin=findViewById(R.id.button_Login);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = correoInicio.getText().toString();
+                contraseña = contraseñaInicio.getText().toString();
+
+                if(!correoInicio.getText().toString().isEmpty() && !contraseñaInicio.getText().toString().isEmpty()){
+                    iniciosesion();
+                }
+            }
+        });
 
     }
     public void irregistro(View v) {
@@ -36,34 +53,34 @@ public class MainActivity extends AppCompatActivity {
         startActivity(registrar);
     }
 
-    public void iniciosesion(View v){
-        mAuth.signInWithEmailAndPassword(correoInicio.getText().toString(),contraseñaInicio.getText().toString())
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    public void iniciosesion(){
+        mAuth.signInWithEmailAndPassword(correoInicio.getText().toString(), contraseñaInicio.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Log.d(TAG, "signInWithEmail:success");
-                            Toast.makeText(getApplicationContext(), "Inicio de sesion exitoso.",
-                                    Toast.LENGTH_SHORT).show();
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Log.d(TAG, "signInWithEmail:success");
+                                Toast.makeText(getApplicationContext(), "Inicio de sesion exitoso.",
+                                        Toast.LENGTH_SHORT).show();
 
-                            // Sign in success, update UI with the signed-in user's information
+                                // Sign in success, update UI with the signed-in user's information
 
-                            Intent continuar=new Intent(MainActivity.this,Navigator.class);
-                            startActivity(continuar);
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(getApplicationContext(), "Error inicio de sesion.",
-                                    Toast.LENGTH_SHORT).show();
-                           // updateUI(null);
+                                Intent continuar = new Intent(MainActivity.this, Navigator.class);
+                                startActivity(continuar);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Error inicio de sesion.",
+                                        Toast.LENGTH_SHORT).show();
+                                // updateUI(null);
+                            }
+
+                            // ...
                         }
-
-                        // ...
-                    }
-                });
+                    });
 
 
     }
